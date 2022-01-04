3d08b07553d115de4c2614e8cdccbbb152d86e8a
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-01-04 19:09:19 +0300

    Code Formating Done

---
+++ app.js
@@ -0,0 +1,41 @@
+// importing express and ejs
+const express = require('express');
+
+const app = express();
+
+// setting ejs with express.js as a by default template engine
+app.set('view engine', 'ejs');
+
+// using static file
+app.use(express.static('public'));
+
+// using urllencoded middleware for using  form data
+app.use(express.urlencoded({ extended: true }));
+
+// using json middleware for using  json data
+app.use(express.json());
+
+const pLangaugeArray = [];
+// home router
+app.get('/', (req, res) => {
+    res.render('pages/home.ejs', { pLangaugeArray, title: 'Home Page' }); // passing data to the EJS Template
+    res.end();
+});
+
+app.post('/', (req, res) => {
+    const { pLangauge } = req.body;
+    pLangaugeArray.push(pLangauge);
+    res.redirect('/'); // redirect to the get Method
+});
+
+// contact router
+app.get('/contact', (req, res) => {
+    res.render('pages/contact.ejs', { title: 'Contact Page' }); // passing data to the EJS Template;
+    res.end();
+});
+
+// listening server
+const port = process.env.port || 8000;
+app.listen(port, () => {
+    console.log(`Server Is Running on Port ${port}`);
+});
---
+++ ejs_introduction.js
@@ -0,0 +1,24 @@
+/*
+    *** EJS(Embedded JavaScript) ***
+        => EJS is a simple templating language which lets we
+        generate HTML markup with plain JavaScript
+
+        *** There are some benefits of EJS
+            => We can use plain JavaScript
+            => Fast Development time
+            => We can get comfortable beacuse it is simple syntax
+            => It's easy to debugging
+
+        *** Some important Tags ***
+                <% : Scriptlet tag for control-flow, no output
+                <%= : Outputs the value into the template(HTML escaped)
+                <%- : Outputs the unescaped value into the template
+                <%# : Comments tag, no execution, no output
+                -%>: Trim-mode ('newline slurp') tag, trims following newline
+
+        *** Includes ***
+            => If we want to includes are relative to the template, we can use it.
+
+    *** Credits by Anisul Islam and Hasan Mahmud Nayem ( Teacher )***
+
+*/
